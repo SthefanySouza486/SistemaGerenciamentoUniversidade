321a9a05420f93a9ec054cb641ebc838865add3d
Sthefany_Souza <sthefany.souza1006@gmail.com>
2025-07-03 16:44:27 -0300

    Tratamento Global de Exceções - Criado

--- src/main/java/com/example/university/entidades/IdCompostoMatricula.java
+++ src/main/java/com/example/university/entidades/IdCompostoMatricula.java
@@ -3,6 +3,7 @@ package com.example.university.entidades;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
@@ -11,8 +12,10 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @Embeddable
+@EqualsAndHashCode
 public class IdCompostoMatricula  implements Serializable {
 
+    private static final long serialVersionUID = 1L;
     private Long studentId;
     private Long cursoId;
 }
