0402708b4ef62bfcacd30f713347031c616b2568
Sthefany_Souza <sthefany.souza1006@gmail.com>
2025-07-04 15:27:28 -0300

    Entidade Escritorio - Criado

---
+++ src/main/java/com/example/university/entidades/Escritorio.java
@@ -0,0 +1,23 @@
+package com.example.university.entidades;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@Entity
+public class Escritorio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String numeroSala;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "professor_id", unique = true)
+    @JsonBackReference
+    @ToString.Exclude
+    private Professor professor;
+}
