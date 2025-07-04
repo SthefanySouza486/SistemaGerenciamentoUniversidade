60b4800b75b058167be51cf69db82bd291162f20
Sthefany_Souza <sthefany.souza1006@gmail.com>
2025-07-04 14:56:29 -0300

    Entidade Estudante - Criado

---
+++ src/main/java/com/example/university/entidades/Estudante.java
@@ -0,0 +1,19 @@
+package com.example.university.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Estudante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+
+}
