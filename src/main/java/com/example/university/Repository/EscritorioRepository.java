b48b75f5b1932f68d4f161bf015c28abe079b09d
Sthefany_Souza <sthefany.souza1006@gmail.com>
2025-07-07 15:54:19 -0300

    Escritorio Repository - criado

---
+++ src/main/java/com/example/university/Repository/EscritorioRepository.java
@@ -0,0 +1,7 @@
+package com.example.university.Repository;
+
+import com.example.university.Entidades.Escritorio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EscritorioRepository extends JpaRepository<Escritorio,Long> {
+}
