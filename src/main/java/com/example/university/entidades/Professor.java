a4665b4b971ff372701e38e864f6a4070c610ae0
Sthefany_Souza <sthefany.souza1006@gmail.com>
2025-07-07 13:47:32 -0300

    Entidade Professor - update

--- src/main/java/com/example/university/entidades/Professor.java
+++ src/main/java/com/example/university/entidades/Professor.java
@@ -3,11 +3,15 @@ package com.example.university.entidades;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Entity
+@EqualsAndHashCode(exclude = {"escritorio", "curso"})
 public class Professor {
 
     @Id
@@ -25,11 +29,28 @@ public class Professor {
     @JsonManagedReference
     private Escritorio escritorio;
 
+    @ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    @JoinTable(name = "professor_cursos",
+            joinColumns = {@JoinColumn(name = "professor_id")},
+            inverseJoinColumns = {@JoinColumn(name = "curso_id")})
+    private Set<Curso> cursos = new HashSet<>();
+
+
     public void setEscritorio(Escritorio escritorio){
         if(escritorio != null){
             escritorio.setProfessor(this);
         }
         this.escritorio = escritorio;
+    }
+
+    public void addCurso(Curso curso){
+        this.cursos.add(curso);
+        curso.getProfessores().remove(this);
+    }
+
+    public void removeCurso(Curso curso){
+        this.cursos.remove(curso);
+        curso.getProfessores().remove(this);
     }
 
 }
