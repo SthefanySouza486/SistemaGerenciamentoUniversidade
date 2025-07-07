6edc808914dbb72707c95005db93be1dc508212a
Sthefany_Souza <sthefany.souza1006@gmail.com>
2025-07-07 16:32:49 -0300

    Impl Service Estudante - criado

---
+++ src/main/java/com/example/university/Impl/EstudanteServiceImpl.java
@@ -0,0 +1,66 @@
+package com.example.university.Impl;
+
+import com.example.university.DTOs.Estudante.EstudanteCreateForm;
+import com.example.university.DTOs.Estudante.EstudanteUpdateForm;
+import com.example.university.Entidades.Estudante;
+import com.example.university.Repository.EstudanteRepository;
+import com.example.university.Services.EstudanteService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class EstudanteServiceImpl implements EstudanteService {
+    private final EstudanteRepository repository;
+    private final EstudanteRepository estudanteRepository;
+
+    public EstudanteServiceImpl(EstudanteRepository repository, EstudanteRepository estudanteRepository) {
+        this.repository = repository;
+        this.estudanteRepository = estudanteRepository;
+    }
+
+    @Override
+    public Estudante create (EstudanteCreateForm form){
+        Estudante estudante = new Estudante();
+        estudante.setNome(form.nome());
+        return repository.save(estudante);
+    }
+
+    @Override
+    public Estudante findById(Long id){
+        return repository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundException("Estudante não encontrado com id " + id));
+    }
+
+    @Override
+    public List<Estudante> findAll(){
+        return repository.findAll();
+    }
+
+    @Override
+    public Estudante update(Long id, EstudanteUpdateForm form) {
+        Estudante estudante = findById(id);
+        if(form.nome() != null && !form.nome().isBlank()){
+            estudante.setNome(form.nome());
+        }
+        return repository.save(estudante);
+    }
+
+    @Override
+    public void delete(Long id) {
+        if(!estudanteRepository.existsById(id)){
+            throw new EntityNotFoundException("Estudante não encontrado com id " + id);
+        }
+        repository.deleteById(id);
+    }
+
+    @Override
+    public List<String> getAllEstudanteNomesUpperCase() {
+        return repository.findAll()
+                .stream()
+                .map(estudante -> estudante.getNome().toUpperCase())
+                .collect(Collectors.toList());
+    }
+}
