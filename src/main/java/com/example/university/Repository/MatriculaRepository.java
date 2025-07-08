1b4a902eaf3d6a450792619e9a086595d8d06d03
Sthefany_Souza <sthefany.souza1006@gmail.com>
2025-07-08 14:34:32 -0300

    Repository Matricula - modificado

--- src/main/java/com/example/university/Repository/MatriculaRepository.java
+++ src/main/java/com/example/university/Repository/MatriculaRepository.java
@@ -1,12 +1,13 @@
 package com.example.university.Repository;
 
+import com.example.university.Entidades.IdMatricula;
 import com.example.university.Entidades.Matricula;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
+public interface MatriculaRepository extends JpaRepository<Matricula, IdMatricula> {
     List<Matricula> findByEstudanteId(Long estudanteId);
     List<Matricula> findByCursoId(Long cursoId);
     List<Matricula> findByMatriculaDateAfter(LocalDateTime data);
